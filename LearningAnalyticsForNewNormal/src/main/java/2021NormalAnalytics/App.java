910f7c41ee216642947b17eadee1677ce10b2f45
Will Tripp <will.tripp@btinternet.com>
2021-10-15 14:27:00 +0100

    Added Maven project

---
+++ LearningAnalyticsForNewNormal/src/main/java/2021NormalAnalytics/App.java
@@ -0,0 +1,13 @@
+//package 2021NormalAnalytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
