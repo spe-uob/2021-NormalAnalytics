947681a8131e43fb547e0863d4775228a863ca3a
Will Tripp <will.tripp@btinternet.com>
2021-10-25 10:27:15 +0100

    Created Hello World!

---
+++ NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/HelloWorldController.java
@@ -0,0 +1,14 @@
+package SPETeam.NormalAnalytics;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
