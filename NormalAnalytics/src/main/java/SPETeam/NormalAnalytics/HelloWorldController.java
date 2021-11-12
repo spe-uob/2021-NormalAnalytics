fa5ed02e2c58dd3c135371df31d5a27a5c4aa890
SianaDicheva <78383370+SianaDicheva@users.noreply.github.com>
2021-11-12 12:50:10 +0000

    Update HelloWorldController.java

--- NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/HelloWorldController.java
+++ NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     @ResponseBody
     public String helloWorld(){
         return "Hello World!";
