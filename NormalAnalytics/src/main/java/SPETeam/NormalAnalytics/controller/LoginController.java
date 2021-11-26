299361ff171da21e14c8e901b3ed86c7c20b2244
Will Tripp <will.tripp@btinternet.com>
2021-11-26 14:23:49 +0000

    Changed default DatabaseReceiver to CustomDatabaseReciever

--- NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/controller/LoginController.java
+++ NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/controller/LoginController.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 public class LoginController {
 
     @Autowired
-    @Qualifier("TestingDatabaseReceiver")
     IDatabaseReceiver receiver;
 
     @PostMapping("/login")
