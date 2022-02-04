63b7efe8e8de5d4a3010363459f9900d2fd88000
Will Tripp <will.tripp@btinternet.com>
2022-02-04 15:21:53 +0000

    Changed generic database names

--- db-init.sql
+++ db-init.sql
@@ -1,44 +1,44 @@
 CREATE TABLE tutor (
-username varchar(100) UNIQUE NOT NULL,
-password varchar(100) NOT NULL,
-firstname varchar(100) NOT NULL,
-surname varchar(100) NOT NULL,
-id int UNIQUE AUTO_INCREMENT,
-PRIMARY KEY (id)
+tutor_username varchar(100) UNIQUE NOT NULL,
+tutor_password varchar(100) NOT NULL,
+tutor_firstname varchar(100) NOT NULL,
+tutor_surname varchar(100) NOT NULL,
+tutor_id int UNIQUE AUTO_INCREMENT,
+PRIMARY KEY (tutor_id)
 );
 
 CREATE TABLE unit (
-id int UNIQUE AUTO_INCREMENT,
+unit_id int UNIQUE AUTO_INCREMENT,
 code varchar(9) UNIQUE NOT NULL,
 unit_name varchar(100) NOT NULL,
-PRIMARY KEY (id)
+PRIMARY KEY (unit_id)
 );
 
 CREATE TABLE student (
-id int UNIQUE AUTO_INCREMENT,
-username varchar(100) UNIQUE NOT NULL,
-firstname varchar(100) NOT NULL,
-surname varchar(100) NOT NULL,
+student_id int UNIQUE AUTO_INCREMENT,
+student_username varchar(100) UNIQUE NOT NULL,
+student_firstname varchar(100) NOT NULL,
+student_surname varchar(100) NOT NULL,
 tutor_id int NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (tutor_id) REFERENCES tutor(id)
+PRIMARY KEY (student_id),
+FOREIGN KEY (tutor_id) REFERENCES tutor(tutor_id)
 );
 
 CREATE TABLE assessment (
-id int UNIQUE AUTO_INCREMENT,
+assessment_id int UNIQUE AUTO_INCREMENT,
 assessment_name varchar(100) UNIQUE NOT NULL,
 summative bool NOT NULL,
 unit_id int NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (unit_id) REFERENCES unit(id)
+PRIMARY KEY (assessment_id),
+FOREIGN KEY (unit_id) REFERENCES unit(unit_id)
 );
 
 CREATE TABLE attendance (
 student_id int NOT NULL,
 unit_id int NOT NULL,
 attendance real NOT NULL,
-FOREIGN KEY (student_id) REFERENCES student(id),
-FOREIGN KEY (unit_id) REFERENCES unit(id),
+FOREIGN KEY (student_id) REFERENCES student(student_id),
+FOREIGN KEY (unit_id) REFERENCES unit(unit_id),
 PRIMARY KEY (student_id, unit_id)
 );
 
@@ -46,15 +46,15 @@ CREATE TABLE grades (
 student_id int UNIQUE NOT NULL,
 assessment_id int UNIQUE NOT NULL,
 grade real NOT NULL,
-FOREIGN KEY (student_id) REFERENCES student(id),
-FOREIGN KEY (assessment_id) REFERENCES assessment(id),
+FOREIGN KEY (student_id) REFERENCES student(student_id),
+FOREIGN KEY (assessment_id) REFERENCES assessment(assessment_id),
 PRIMARY KEY (student_id, assessment_id)
 );
 
 CREATE TABLE unit_enrollment(
 student_id int UNIQUE NOT NULL,
 unit_id int UNIQUE NOT NULL,
-FOREIGN KEY (student_id) REFERENCES student(id),
-FOREIGN KEY (unit_id) REFERENCES unit(id),
+FOREIGN KEY (student_id) REFERENCES student(student_id),
+FOREIGN KEY (unit_id) REFERENCES unit(unit_id),
 PRIMARY KEY (student_id,unit_id)
 );
