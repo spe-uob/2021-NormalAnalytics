fc6ad077643a278c501adc63208cec5662787e05
Luo <kk19041@bristol.ac.uk>
2022-02-07 15:07:34 +0800

    add some security configurations

---
+++ NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package SPETeam.NormalAnalytics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //Close csrf
+                .csrf().disable()
+                //Get SecurityContext without Session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // For login interface, anonymous access is allowed
+                .antMatchers("/user/login").anonymous()
+                // All requests other than the above require authentication
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
