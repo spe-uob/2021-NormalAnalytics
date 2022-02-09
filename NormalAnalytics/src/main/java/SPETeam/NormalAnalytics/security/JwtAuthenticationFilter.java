1faac23a073ac8dea873cc9e28c85b5d5f567530
Luo <kk19041@bristol.ac.uk>
2022-02-09 12:01:39 +0800

    get the token from the request header

--- NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/security/JwtAuthenticationFilter.java
+++ NormalAnalytics/src/main/java/SPETeam/NormalAnalytics/security/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package SPETeam.NormalAnalytics.security;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +13,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText(token)) {
+            //Release
+            filterChain.doFilter(request, response);
+            return;
+        }
 
     }
 }
