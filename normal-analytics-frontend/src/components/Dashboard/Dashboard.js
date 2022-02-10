161fbeda49a7c592cca131f924cf7c4e5d40f3ae
Sam Thomas <ne20327@bristol.ac.uk>
2022-02-10 17:53:27 +0000

    refactor: change component to functional-based rather than class-based to allow for use of hooks

--- normal-analytics-frontend/src/components/Dashboard/Dashboard.js
+++ normal-analytics-frontend/src/components/Dashboard/Dashboard.js
@@ -1,122 +1,57 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
-import 'react-dropdown/style.css';
+import Sidebar from "../SideBar/SideBar";
 import "./Dashboard.css"
-import Sidebar from '../SideBar/SideBar';
-import ReactTable from "react-table";  
-import Table from '../TableContainer';
 
-class Dashboard extends React.Component {
-    passedState = null;
+function DashboardComponent(props) {
+    let passedState = props.location.state;
 
-    handleClick = () => {
-        this.props.history.push({
+    let handleClick = () => {
+        props.history.push({
             pathname: '/student-auth',
-            state: this.passedState
+            state: passedState
         })
     }
 
-    handleClickLogin = () => {
-        this.props.history.push({
+    let handleClickLogin = () => {
+        props.history.push({
             pathname: '/login',
-            state: this.passedState
+            state: passedState
         })
     }
 
-    render() {
-        //https://www.bacancytechnology.com/blog/react-table-tutorial-part-1/#1
-        //used this tutorial, instead of hard-coding use UseEffect to fetch it, just didnt have time
-        //if you want leave the styling to me
-        //there is a separate file called TableContainer used to hold the table 
-        // i have moved the sidebar into its own component, not sure for the navigation bar
+    let studentObjects = passedState["tutorAndTutees"]["studentObjects"];
+    let studentName = passedState["studentUsername"]["value"];
+    let tutorUsername = passedState["tutorAndTutees"]["tutorUsername"]
 
-        /*const [data, setData] = useState([]);
- 
-        useEffect(() => {
-        const {state} = this.props.location;
-        this.passedState = state;
-            .then((res) => {
-            setData(res.data);
-            })
-            .catch((err) => console.log(err))
-        }, []);
-        */
-        const data = 
-        [
-        {
-        name: 'Computer systems A',
-        address: '20',
-        date: '07/12/2021',
-        order: '67',
-        },
-        {
-        name: 'Algorithms II',
-        address: '10',
-        date: '07/11/2021',
-        order: '85',
-        },
-        ]
-
-        const columns =  [
-            {
-            Header: 'Units ',
-            columns: [
-            {
-            Header: 'Name',
-            accessor: 'name',
-            },
-            {
-            Header: 'Credit points',
-            accessor: 'address',
-            },
-            {
-             Header: 'Score',
-             accessor: 'order',
-             },
-            ],
-           },
-        ]
-        
-        const {state} = this.props.location;
-        this.passedState = state;
-        let myObj = JSON.stringify(state);
-        let myNewObj = JSON.parse(myObj);
-
-        let studentObjects = myNewObj["tutorAndTutees"]["studentObjects"];
-        console.log(studentObjects);
-        let studentName = myNewObj["studentUsername"]["value"]
-        let currentStudentName = null;
-
-        for (const [key, value] of Object.entries(studentObjects)) {
-            if (studentName === key) {
-                console.log(value);
-                currentStudentName = key
-            }
+    let studentUsername = null;
+    for (const [key, value] of Object.entries(studentObjects)) {
+        if (studentName === key) {
+            studentName = key
+            studentUsername = value;
         }
-
-        let tutorName = myNewObj["tutorAndTutees"]["tutorUsername"];
-
-        return (
-            <div className="dashboard">
-                
-                <div className="nav-bar">
-                    <button className="nav-item left" onClick={this.handleClick.bind(this)}>Change Student</button>
-                    <button className="nav-item">Current student: {currentStudentName}</button>
-                    <div className="dropdown">
-                        <button className="nav-item" style={{border: "solid black"}} >Tutor logged in: {tutorName}</button>
-                        <div className="dropdown-content">
-                            <a className="log-out"  onClick={this.handleClickLogin.bind(this)}>Log Out</a>
-                        </div>
-                    </div>
-                </div>
-                <Sidebar /> 
-                <div>  
-                <Table columns={columns} data={data} /> 
-                </div>  
-              
-            </div>
-        );
     }
+
+    return (
+        <div className="dashboard">
+
+         <div className="nav-bar">
+            <button className="nav-item left" onClick={handleClick.bind(this)}>Change Student</button>
+             <button className="nav-item">Current student: {studentName}</button>
+             <div className="dropdown">
+                 <button className="nav-item" style={{border: "solid black"}} >Tutor logged in: {tutorUsername}</button>
+                <div className="dropdown-content">
+                     <a className="log-out" onClick={handleClickLogin.bind(this)}>Log Out</a>
+                 </div>
+             </div>
+         </div>
+
+         <div className="dashboard-content">
+             <Sidebar />
+         </div>
+
+        </div>
+    );
 }
 
-export default withRouter (Dashboard);
+export default withRouter(DashboardComponent);
